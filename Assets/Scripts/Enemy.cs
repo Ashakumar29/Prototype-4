8b9c2b9e0486f4fcad7c6f19d235b42f0b975586
Ashakumar29 <75035559+Ashakumar29@users.noreply.github.com>
2023-01-04 21:52:01 +0000

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent & lt; Rigidbody & gt; ();
+        player = GameObject.Find(&quot; Player & quot;);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y & lt; -10)
+{
+
+            Destroy(gameObject);
+        }
+    }
+}
